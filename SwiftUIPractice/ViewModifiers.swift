fd59ed4731c6094a8108db4e1175a4ae3fedc5b0
Davit Natenadze <davit.natenadze@gmail.com>
2024-04-10 00:30:32 +0200

    viewModifier

---
+++ SwiftUIPractice/ViewModifiers.swift
@@ -0,0 +1,50 @@
+//
+//  ViewModifiers.swift
+//  SwiftUIPractice
+//
+//  Created by Davit Natenadze on 10.04.24.
+//
+
+import SwiftUI
+
+struct CustomButtonModifier: ViewModifier {
+    
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(width: 200)
+            .font(.title2)
+            .foregroundStyle(.white)
+            .background(color)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+extension View {
+    func withCustomButtonModifier(color: Color = .blue) -> some View {
+        modifier(CustomButtonModifier(color: color))
+    }
+    
+}
+
+struct ViewModifiers: View {
+    
+    var body: some View {
+        Button("Start") {
+            print("start pressed")
+        }
+        .modifier(CustomButtonModifier(color: .orange))
+        
+        Button("Finish") {
+            print("FINISH pressed")
+        }
+        .withCustomButtonModifier(color: .red)
+    }
+}
+
+
+#Preview {
+    ViewModifiers()
+}
